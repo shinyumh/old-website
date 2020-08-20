86f755e140d0a51f13e1454c0098c077fdfb3af1
Melody Hu <melodyhu2003@gmail.com>
2020-08-19 18:22:00 -0700

    php files for form

---
+++ settings.php
@@ -0,0 +1,14 @@
+<?php
+
+/* Settings
+---------------------*/
+$email_to = 'melodyhu2003@gmail.com'; // Email addresses to send data. Seperate email addresses with ','
+$site_title = 'Melody Hu'; // Your website title
+$webmaster_name = "Melody Hu"; // Your name
+
+$headerbar_color = "#000000"; // Color of the headerbar in the email sent
+$form_type = "contact form"; // Type of form - default is Contact Form
+
+$success_redirect = "https://shinyumh.github.io"; // Page to redirect to after submission of form.
+
+?>
